d0b38f1f8be78f358e9a33e5a27b5f7ca805273c
MarkMathiasz <mathiasz.m@gmail.com>
2021-04-25 15:41:53 +0200

    Feat add form handle to login

--- ov/src/app/logIn/logIn.component.ts
+++ ov/src/app/logIn/logIn.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-logIn',
@@ -10,11 +12,33 @@ export class LogInComponent implements OnInit {
   visible: boolean = true;
   name: string;
 
-  constructor(private router: Router) { }
+  loginForm: FormGroup;
+  user: User;
+
+  formValue = {
+    username: ['', [Validators.required, Validators.minLength(4)]],
+    password: ['', [Validators.required, Validators.minLength(8),]],
+  }
+
+  constructor(private router: Router,
+    private fb: FormBuilder) {
+  }
 
   ngOnInit() {
-    this.name="valami"
+    this.loginForm = this.fb.group(this.formValue);
   }
+
+  get username() {
+    return this.loginForm.get('username')
+  }
+  get password() {
+    return this.loginForm.get('password')
+  }
+  onSubmit() {
+    if (this.loginForm.valid)
+      console.log(this.loginForm.value)
+  };
+
   goToReg() {
     this.visible = false;
     setTimeout(() => {
