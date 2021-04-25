579c5e541ddcc0b122cc2eec5f1b7a6bfe92645b
MarkMathiasz <mathiasz.m@gmail.com>
2021-04-25 15:40:22 +0200

    Fix edit user form

--- ov/src/app/edit-user/edit-user.component.ts
+++ ov/src/app/edit-user/edit-user.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -15,10 +14,10 @@ export class EditUserComponent implements OnInit, OnDestroy {
   myForm: FormGroup;
   user: User;
 
-  formValue ={
-    username: ['',[Validators.required, Validators.maxLength(4)]],
-    email:['',[Validators.required,Validators.email]],
-    password: ['',[Validators.required]],
+  formValue = {
+    username: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8),]],
     newPassword1: '',
     newPassword2: '',
   }
@@ -30,33 +29,33 @@ export class EditUserComponent implements OnInit, OnDestroy {
     this.myForm = this.fb.group(this.formValue);
   }
 
-get email(){
-  return this.myForm.get('email')
-}
-get username(){
-  return this.myForm.get('username')
-}
-get password(){
-  return this.myForm.get('password')
-}
-get newPassword1(){
-  return this.myForm.get('newPassword1')
-}
-get newPassword2(){
-  return this.myForm.get('newPassword2')
-}
+  get email() {
+    return this.myForm.get('email')
+  }
+  get username() {
+    return this.myForm.get('username')
+  }
+  get password() {
+    return this.myForm.get('password')
+  }
+  get newPassword1() {
+    return this.myForm.get('newPassword1')
+  }
+  get newPassword2() {
+    return this.myForm.get('newPassword2')
+  }
 
 
   onSubmit() {
 
- if(this.myForm.valid)
-console.log(this.myForm.value)
+    if (this.myForm.valid)
+      console.log(this.myForm.value)
 
 
   };
 
   ngOnDestroy(): void {
- 
+
   }
 }
 
