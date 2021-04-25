79bb9e059210f2ff427e569cbaaf3652520064ff
MarkMathiasz <mathiasz.m@gmail.com>
2021-04-25 16:09:48 +0200

    Add registration form

--- ov/src/app/registration/registration.component.ts
+++ ov/src/app/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../models/user.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,11 +9,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+  user: User;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fb: FormBuilder) { }
   visible: boolean = true;
-  ngOnInit() {
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group(this.formValue);
   }
+
+  formValue = {
+    username: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    password1: ['', [Validators.required, Validators.minLength(8),]],
+    password2: '',
+  }
+
+
+  get email() {
+    return this.registrationForm.get('email')
+  }
+  get username() {
+    return this.registrationForm.get('username')
+  }
+  get password1() {
+    return this.registrationForm.get('password1')
+  }
+  get password2() {
+    return this.registrationForm.get('password2')
+  }
+  onSubmit() {
+
+    if (this.registrationForm.valid)
+      console.log(this.registrationForm.value)
+
+
+  };
 
   goToLogin() {
     this.visible = false;
